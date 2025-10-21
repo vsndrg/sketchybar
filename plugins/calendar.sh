51076bc251f1b3bafcc37d33b75e659a534e3381
Egor Denisov <vsndrg@Egors-MacBook-Pro.local>
2025-10-21 22:13:00 +0300

    add new config

---
+++ plugins/calendar.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# #sketchybar --set $NAME icon="$(date '+%a %d. %b')" label="$(date '+%H:%M')"
+# sketchybar --set "$NAME" label="$(date '+%m/%d %H:%M')"
+
+#!/bin/bash
+
+sketchybar --set $NAME label="$(date +'%a %d %b %H:%M')"
